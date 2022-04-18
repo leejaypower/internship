390dfa73a68be6b274764b891036cbb5a7643491
juyoung jung <maliethy@gmail.com>
2022-04-19 01:02:37 +0900

    [frontend/juyoungjung][#7] vue cli로 vue init하기  (#12)
    * [frontend/juyoungjung][#7] vue init - vue-cli를 통해 vue@2 project init하기(정주영)
    
    * [frontend/juyoungjung][#7] Eslint 세팅 - Frontend 공통 적용(정주영)
    
    * [frontend/juyoungjung][#7] .eslintrc.js rules indent 설정 삭제하기, vue@3->vue@2로 변경하기, eslint error 확인 후 수정하기(정주영)
    
    * [frontend/juyoungjung][#7] .eslintrc.js airbnb와 중복되는 설정 및 인턴 프론트엔드 공통적용 설정 삭제하기(정주영)

---
+++ frontend/juyoungjung/intro/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
